d743fdff42d97e6ec3de3bb926376041e149599d
Thayna <thaynadesantana@outlook.com>
2023-08-04 18:35:30 -0300

    finally os ultimos ajustes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { NaoEncontradaComponent } from './nao-encontrada/nao-encontrada.component';
 import { DetalhesPlantasComponent } from './plantas/detalhes-plantas/detalhes-plantas.component';
 
-const routes: Routes = [{ path: 'plantas', loadChildren: () => import('./plantas/plantas.module').then(m => m.PlantasModule) },
+const routes: Routes = [
+{ path: 'plantas', loadChildren: () => import('./plantas/plantas.module').then(m => m.PlantasModule) },
 { path: "", redirectTo: "plantas", pathMatch: 'full'},
 { path: 'carrinho', loadChildren: () => import('./carrinho/carrinho.module').then(m => m.CarrinhoModule) },
 { path: 'contato', loadChildren: () => import('./contato/contato.module').then(m => m.ContatoModule) },
@@ -11,6 +12,7 @@ const routes: Routes = [{ path: 'plantas', loadChildren: () => import('./plantas
 ];
 
 @NgModule({
+  declarations: [],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
